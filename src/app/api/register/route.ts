046692ccad0c4019f58ed0ba2cb3e5ba8f171b5f
AboElhasen <boukhrisabdessamedq@gmail.com>
2026-05-19 23:54:43 +0100

    Update POST route to exclude 'groupe' field
    Removed 'groupe' from the request body destructuring.

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,7 +5,7 @@ import bcrypt from "bcryptjs";
 export async function POST(req: Request) {
     try {
         const body = await req.json(); // قراءة البيانات من Body الطلب
-        const { Mat, fullname, email, groupe, password } = body;
+        const { Mat, fullname, email, password } = body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const student = await prisma.student.create({
             data: {
