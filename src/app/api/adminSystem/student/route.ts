eb092fbd6224aeb730001bbd4647ca6d5dd7d048
AboElhasen <boukhrisabdessamedq@gmail.com>
2026-05-20 01:43:03 +0100

    Update import path for getServerSession

--- src/app/api/adminSystem/student/route.ts
+++ src/app/api/adminSystem/student/route.ts
@@ -1,4 +1,4 @@
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 import { NextResponse } from "next/server";
 import  prisma  from  "../../../../../prisma/client";
 
